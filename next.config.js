ddd84d7042f57287cb07a385f1d2d8c512fc8e01
Ninja <72120564+Nowfel1@users.noreply.github.com>
2026-06-12 12:31:15 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400",
+          },
+        ],
+      },
+      {
+        source: "/robots.txt",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain",
+          },
+        ],
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
