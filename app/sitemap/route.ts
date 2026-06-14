dde79501ec9aeff5de277d737892ce2c7444cf0c
Ninja <72120564+Nowfel1@users.noreply.github.com>
2026-06-14 22:55:40 +0600

    Update route.ts

--- app/sitemap/route.ts
+++ app/sitemap/route.ts
@@ -5,9 +5,33 @@ export async function GET() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://tokensfund.xyz/</loc>
-    <lastmod>2026-06-12</lastmod>
+    <lastmod>2026-06-14</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://tokensfund.xyz/blog</loc>
+    <lastmod>2026-06-14</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://tokensfund.xyz/blog/cross-chain-swap-without-kyc-2026</loc>
+    <lastmod>2026-06-13</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://tokensfund.xyz/blog/swap-xmr-btc-no-kyc</loc>
+    <lastmod>2026-06-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://tokensfund.xyz/blog/btc-to-eth-without-kyc-2026</loc>
+    <lastmod>2026-06-14</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
   </url>
 </urlset>`;
 
