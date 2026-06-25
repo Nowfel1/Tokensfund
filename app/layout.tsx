ad16a964348f6061b607d3b19eae2a56c5f0a1c6
Ninja <72120564+Nowfel1@users.noreply.github.com>
2026-06-25 20:40:48 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>{children}</body>
+       <Footer />
     </html>
   );
 }
