044e5b051b7413728f8ec7cbeae04522e63046a0
Ninja <72120564+Nowfel1@users.noreply.github.com>
2026-07-09 17:19:32 +0600

    Update route.ts

--- app/sitemap/route.ts
+++ app/sitemap/route.ts
@@ -39,6 +39,12 @@ export async function GET() {
     <priority>0.3</priority>
   </url>
   <url>
+    <loc>https://tokensfund.xyz/blog/stablecoin-flippening-usdc-usdt-2026</loc>
+    <lastmod>2026-07-07</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
     <loc>https://tokensfund.xyz/blog/best-btc-to-xmr-rate-2026</loc>
     <lastmod>2026-07-06</lastmod>
     <changefreq>monthly</changefreq>
