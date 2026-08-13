cef2e48aeaa32f8ff96774b90e43c63acec5af19
Tokensfund <72120564+Nowfel1@users.noreply.github.com>
2026-08-13 18:20:43 +0600

    Update page.tsx

--- app/blog/binance-usdt-eu-mica-delisting-2026/page.tsx
+++ app/blog/binance-usdt-eu-mica-delisting-2026/page.tsx
@@ -164,7 +164,7 @@ export default function Post() {
         <div className="blog-cta">
           <p>Swap from a wallet you control</p>
           <Link
-            href="/"
+            href="/swap/btc-to-eth"
             style={{
               display: "inline-block",
               textDecoration: "none",
